355747ec36a8464f018bc683a4c4dfe0f010b518
Kyle Oliveiro <kyle@watchtower.digital>
2018-11-22 17:16:47 +0800

    Add lazysizes-config.js

---
+++ src/scripts/lazysizes-config.js
@@ -0,0 +1,10 @@
+///
+// Configure lazysizes
+// ===================
+// Info: https://github.com/aFarkas/lazysizes
+///
+
+(function() {
+  window.lazySizesConfig = window.lazySizesConfig || {};
+  window.lazySizesConfig.throttleDelay = 100;
+})();

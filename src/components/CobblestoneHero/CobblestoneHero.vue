fb94c9edc6c7f1e2d38201c827c8474e0f52b655
Kyle Oliveiro <kyle@watchtower.digital>
2018-12-06 15:11:40 +0800

    Fix CobblestoneHero layout in IE

--- src/components/CobblestoneHero/CobblestoneHero.vue
+++ src/components/CobblestoneHero/CobblestoneHero.vue
@@ -1,29 +1,27 @@
 <template>
-  <div class="d:f w:100 h:100vh">
-    <div class="m:a">
-      <div class="typeset contain ta:c nm-t:4xl">
-        <img
-          class="lazyload"
-          src="/_assets/images/cobblestone.png"
-          alt=""
-          @click="_handleClick"
-        />
+  <div class="d:f ai:c jc:c w:100 h:100vh">
+    <div class="typeset contain ta:c nm-t:4xl">
+      <img
+        class="lazyload"
+        src="/_assets/images/cobblestone.png"
+        alt=""
+        @click="_handleClick"
+      />
 
-        <h1>Welcome to {{ projectName }}!</h1>
+      <h1>Welcome to {{ projectName }}!</h1>
 
-        <p v-if="showHelp">
-          You should start with the
-          <a
-            href="https://github.com/PebbleRoad/cobblestone"
-            target="_blank"
-            noopener
-            noreferrer
-          >
-            <strong>README</strong>
-          </a>
-          :)
-        </p>
-      </div>
+      <p v-if="showHelp">
+        You should start with the
+        <a
+          href="https://github.com/PebbleRoad/cobblestone"
+          target="_blank"
+          noopener
+          noreferrer
+        >
+          <strong>README</strong>
+        </a>
+        :)
+      </p>
     </div>
   </div>
 </template>
